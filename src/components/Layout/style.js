979c30f38fac59f8d9542f8237c69dfbed42d716
DakoMumladze <dako.dako.mumladze@gmail.com>
2020-07-09 14:47:34 +0400

    Add layout component UI

---
+++ src/components/Layout/style.js
@@ -0,0 +1,11 @@
+import { makeStyles } from '@material-ui/core/styles'
+
+export default makeStyles((theme) => ({
+    main: {
+      display: 'flex',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+      color: '#fff'
+    }
+}))
+
